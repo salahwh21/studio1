edb6ac408818bfb2a7a26dc5a030f4592d86d825
Salah Alhasany <abuahmad21@gmail.com>
2025-08-27 09:25:53 +0000

    تكبير الايقونة للواتس أكثر قليلا

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function LoginPage() {
               <div className="flex justify-center gap-4">
                 {settings.socialLinks.whatsapp && (
                   <a href={`https://wa.me/${settings.socialLinks.whatsapp}`} target="_blank" rel="noopener noreferrer">
-                    <Button variant="ghost" size="icon" className="hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"><WhatsappIcon className="h-7 w-7"/></Button>
+                    <Button variant="ghost" size="icon" className="hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"><WhatsappIcon className="h-8 w-8"/></Button>
                   </a>
                 )}
                 {settings.socialLinks.instagram && (
