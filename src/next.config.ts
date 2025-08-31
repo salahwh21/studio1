76e6dbb326c17cb84aa666a820a6ce11e9f86665
Salah Alhasany <abuahmad21@gmail.com>
2025-08-31 19:37:36 +0000

    npm run dev

---
+++ src/next.config.ts
@@ -0,0 +1,45 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    allowedDevOrigins: [
+        "https://*.google.com",
+        "https://*.cloud.google.com",
+        "https://*.run.app",
+        "https://*.web.app",
+        "https://*.firebaseapp.com"
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
