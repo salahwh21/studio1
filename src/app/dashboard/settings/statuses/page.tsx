5cf18844df7855900381c3d394e14a7f30972549
Salah Alhasany <abuahmad21@gmail.com>
2025-08-31 18:43:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/settings/statuses/page.tsx
+++ src/app/dashboard/settings/statuses/page.tsx
@@ -8,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
   CardContent,
+  CardDescription,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import Icon from '@/components/icon';
