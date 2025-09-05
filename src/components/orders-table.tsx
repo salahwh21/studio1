3c9c37b053d1bda128d644d36b217d3ab18e63ef
Salah Alhasany <abuahmad21@gmail.com>
2025-09-05 05:41:04 +0000

    لا حول ولا قوة الا بالله

--- src/components/orders-table.tsx
+++ src/components/orders-table.tsx
@@ -449,9 +449,9 @@ const OrdersTableComponent = () => {
                              const sInfo = getStatusInfo(value as string);
                              content = (
                                  <Select value={value as string} onValueChange={(newValue) => handleUpdateField(order.id, 'status', newValue)}>
-                                     <SelectTrigger className="bg-transparent border-0 focus:ring-0 focus:ring-offset-0 h-8 p-0 w-[180px]">
-                                         <SelectValue>
-                                             <div className="flex items-center justify-center font-semibold text-xs px-2.5 py-0.5 rounded-sm w-full h-full" style={{ backgroundColor: `${sInfo.color}20`, color: sInfo.color }}>
+                                     <SelectTrigger className="bg-transparent border-0 focus:ring-0 focus:ring-offset-0 h-8 p-0">
+                                         <SelectValue asChild>
+                                             <div className="flex items-center justify-center font-semibold text-xs px-2.5 py-0.5 rounded-sm h-full" style={{ width: 'max-content', margin: 'auto', backgroundColor: `${sInfo.color}20`, color: sInfo.color }}>
                                                  <Icon name={sInfo.icon as any} className="h-3 w-3 ml-1.5"/>
                                                  {sInfo.name}
                                              </div>
