907e19e8aaa21858f4bba288c040b4de97715583
Salah Alhasany <abuahmad21@gmail.com>
2025-09-05 01:02:14 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/components/drivers-map.tsx
+++ src/components/drivers-map.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
