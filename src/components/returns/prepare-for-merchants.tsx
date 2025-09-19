c4b19e70de80ca8242e0e32c0fb0fb2919a255b1
Salah Alhasany <abuahmad21@gmail.com>
2025-09-19 20:48:43 +0000

    Try fixing this error: `Runtime ReferenceError: DialogDescription is not

--- src/components/returns/prepare-for-merchants.tsx
+++ src/components/returns/prepare-for-merchants.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Badge } from '@/components/ui/badge';
 import Icon from '@/components/icon';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
 import { useToast } from '@/hooks/use-toast';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
