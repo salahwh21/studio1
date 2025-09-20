dbfeb2b4a653f97631adf214be6269fab93fb08b
Salah Alhasany <abuahmad21@gmail.com>
2025-09-20 20:48:40 +0000

    كيفية حل التحذير
    افتح ملف next.config.js (أو next.config.mjs).
    
    أضف الخ

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,11 +1,19 @@
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    appDir: true,
+  },
   // Your Next.js configuration options go here.
   // For example:
   // images: {
   //   domains: ['example.com'],
   // },
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'https://3000-firebase-al-wameed-delivery-1-1757800056570.cluster-zj37zwdounao2uiqvtma64veco.cloudworkstations.dev'
+  ],
 };
 
 export default nextConfig;
